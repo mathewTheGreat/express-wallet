7a10ca4295e3a229d60d3306399a2c750f570f8b
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 11:08:29 +0300

    update app.js
    change response text to my name to be used on the front end of the app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send('hello from the wallet backend'));
+app.get("/", (req, res) => res.send('Mathew Momanyi'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
